6bdfa018b6ee4614ab99543aaa4e2be0979d9486
Kirill Tolkun <tolkun_14@inbox.ru>
2020-10-12 15:46:34 +0300

    Add fields 'entityCols', 'classCols' to class 'TableConfig'

--- src/app/table/model/table-config.model.ts
+++ src/app/table/model/table-config.model.ts
@@ -1,2 +1,7 @@
+import {EntityColumnConfig} from "./entity-column-config.model";
+import {ClassColumnConfig} from "./class-column-config.model";
+
 export class TableConfig {
+  entityCols: Array<EntityColumnConfig>;
+  classCols: Array<ClassColumnConfig>;
 }
