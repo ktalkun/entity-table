28e718a862aaa950efa12f44e687fa2709e7f519
Kirill Tolkun <tolkun_14@inbox.ru>
2020-10-13 10:40:32 +0300

    Add component 'input-text'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {TableComponent} from './table/table.component';
+import { InputTextComponent } from './input-text/input-text.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableComponent
+    TableComponent,
+    InputTextComponent
   ],
   imports: [
     BrowserModule
