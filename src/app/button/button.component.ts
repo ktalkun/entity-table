f153d4d844e83fe3b2f482747d08d28641a31dac
Kirill Tolkun <tolkun_14@inbox.ru>
2020-10-15 16:50:24 +0300

    Refactor: rename output property 'buttonClick' -> 'click'

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -7,7 +7,7 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   @Input() isDisabled: boolean;
-  @Output() buttonClick = new EventEmitter();
+  @Output('click') buttonClick = new EventEmitter();
 
   constructor() {
   }
