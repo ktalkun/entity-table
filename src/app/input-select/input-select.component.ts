93a872b6d7933cf4758b44ce974ca5c60d657001
Kirill Tolkun <tolkun_14@inbox.ru>
2020-10-15 16:35:10 +0300

    Implement interface ControlValueAccessor

--- src/app/input-select/input-select.component.ts
+++ src/app/input-select/input-select.component.ts
@@ -1,11 +1,32 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, forwardRef, Input, OnInit} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 
 @Component({
   selector: 'app-input-select',
   templateUrl: './input-select.component.html',
-  styleUrls: ['./input-select.component.css']
+  styleUrls: ['./input-select.component.css'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => InputSelectComponent),
+    multi: true
+  }]
 })
-export class InputSelectComponent implements OnInit {
+export class InputSelectComponent implements OnInit, ControlValueAccessor {
+  private val: string;
+
+  get value() {
+    return this.val;
+  }
+
+  set value(value: string) {
+    this.val = value;
+    this.onChange(this.val);
+    this.onTouch(this.val);
+  }
+
+  @Input() options: Array<string>;
+
+  @Input("disabled") isDisabled: boolean;
 
   constructor() {
   }
@@ -13,4 +34,23 @@ export class InputSelectComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  private onChange: Function;
+
+  private onTouch: Function;
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  writeValue(obj: any): void {
+    this.val = obj;
+  }
 }
