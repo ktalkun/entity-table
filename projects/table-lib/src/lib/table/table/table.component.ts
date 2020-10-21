2534016b65a2be4b4e0e05bc5f8ecc8b312844c6
Kirill Tolkun <tolkun_14@inbox.ru>
2020-10-21 17:35:24 +0300

    Set type of property 'config' of 'table' component from 'table-lib'

--- projects/table-lib/src/lib/table/table/table.component.ts
+++ projects/table-lib/src/lib/table/table/table.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {EntityColumnType} from './model/entity-column-type.model';
 import {ClassColumnType} from './model/class-column-type.model';
+import {TableConfig} from './model/table-config.model';
 
 @Component({
   selector: 'tl-table',
@@ -8,7 +9,7 @@ import {ClassColumnType} from './model/class-column-type.model';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent {
-  @Input() config: object;
+  @Input() config: TableConfig;
   @Input() data: Array<object>;
   readonly entityColumnType: typeof EntityColumnType = EntityColumnType;
   readonly classColumnType: typeof ClassColumnType = ClassColumnType;
